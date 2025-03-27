5bb8dfc231167cc7be4f190ea1f3b125e58e1017
Sergio <Sergiopadilla0987@gmail.com>
2025-03-27 15:50:23 -0400

    completed etch a sketch assignment

---
+++ main.js
@@ -0,0 +1,31 @@
+const container = document.querySelector(".container");
+
+const btn = document.createElement("button");
+document.body.insertBefore(btn, container);
+btn.textContent = "hello"
+
+btn.addEventListener("click", () => {
+
+    let askNumSquares = prompt("How many squares per side? (Max: 100)");
+    askNumSquares = parseInt(askNumSquares);
+    if(askNumSquares > 100){
+        askNumSquares = 100;
+    }
+    createSquares(askNumSquares);
+});
+
+const createSquares = (amount) => {
+    container.textContent = '';
+    container.style.width = ((amount - 1) * 51) + "px";
+    for(let i = 0; i < amount * amount; i++){
+        const div = document.createElement("div");
+        div.className = 'square';
+        container.appendChild(div);
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "lightgreen";
+        });
+        /*div.addEventListener("mouseout", () => {
+            div.style.backgroundColor = "coral";
+        });*/
+    }
+};
